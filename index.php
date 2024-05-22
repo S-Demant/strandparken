2dc70d8fc059ed87a779e1e1c0b405b45d9aaa16
Sebastian <sec001@edu.zealand.dk>
2024-05-22 23:23:12 +0200

    Attractions on index

--- index.php
+++ index.php
@@ -40,11 +40,11 @@ require "settings/init.php";
 
 <div class="container position-relative">
     <div class="row">
-        <h2 class=" mt-5">Aktiviteter</h2>
-        <div class="col-12 col-lg-6 mt-3 pe-4">
+        <h2 class="mt-5">Aktiviteter</h2>
+        <div class="col-12 col-lg-6 mt-3 pe-lg-4">
             <a href="#"><img src="img/image.webp" class="img-fluid w-100"></a>
         </div>
-        <div class="col-12 col-lg-6 mt-3 ps-4">
+        <div class="col-12 col-lg-6 mt-3 ps-lg-4">
             <a href="#" class="link-dark"><h2>Titel på aktiviteten</h2></a>
             <span>23-06-2024</span>
             <p class="mt-2">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s. Ipsum passages, and more recently with desktop.</p>
@@ -71,29 +71,34 @@ require "settings/init.php";
     </div>
 </div>
 
-<br>
-<br>
-<br>
-<br>
-<br><p>Hallo</p>
-<br>
-<br>
-<br>
-<br>
-
-<br>
-<br>
-<br>
-<br>
-<br><br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br><br>
-<br>
-<br>
+<div class="container position-relative mt-5 mt-lg-3">
+    <div class="row">
+        <h2 class="mt-5">Attraktioner</h2>
+        <div class="col-12 col-lg-6 mt-3 pe-lg-4">
+            <a href="#"><img src="img/image.webp" class="img-fluid w-100"></a>
+        </div>
+        <div class="col-12 col-lg-6 mt-3 ps-lg-4">
+            <a href="#" class="link-dark"><h2>Titel på aktiviteten</h2></a>
+            <p class="mt-3">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s. Ipsum passages, and more recently with desktop.</p>
+            <a href="#">Læs mere</a>
+        </div>
+        <div class="col-12 col-lg-4 mt-5 mt-lg-4 d-flex flex-column position-relative">
+            <img src="img/image.webp" class="img-fluid w-100">
+            <h2 class="mt-3">Titel på aktiviteten</h2>
+            <a href="#" class="stretched-link">Læs mere</a>
+        </div>
+        <div class="col-12 col-lg-4 mt-5 mt-lg-4 d-flex flex-column position-relative">
+            <img src="img/image.webp" class="img-fluid w-100">
+            <h2 class="mt-3">Titel på aktiviteten</h2>
+            <a href="#" class="stretched-link">Læs mere</a>
+        </div>
+        <div class="col-12 col-lg-4 mt-5 mt-lg-4 d-flex flex-column position-relative">
+            <img src="img/image.webp" class="img-fluid w-100">
+            <h2 class="mt-3">Titel på aktiviteten</h2>
+            <a href="#" class="stretched-link">Læs mere</a>
+        </div>
+    </div>
+</div>
 
 
 
