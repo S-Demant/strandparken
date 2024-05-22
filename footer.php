92ad22ad121e16c32f873073f46549b4210f0a7b
Sebastian <sec001@edu.zealand.dk>
2024-05-22 23:36:17 +0200

    Footer created

---
+++ footer.php
@@ -0,0 +1,3 @@
+<div class="d-flex justify-content-center my-5">
+    <a href="index.php"><img src="img/logo-footer.webp" class="img-fluid"></a>
+</div>
--- index.php
+++ index.php
@@ -100,7 +100,9 @@ require "settings/init.php";
     </div>
 </div>
 
-
+<footer>
+    <?php include("footer.php"); ?>
+</footer>
 
 <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 </body>
