a312c0a254e33209413f422e343547db03076c03
Sebastian <sec001@edu.zealand.dk>
2024-05-22 23:14:32 +0200

    Activities on index

--- index.php
+++ index.php
@@ -36,15 +36,51 @@ require "settings/init.php";
     </div>
 </div>
 
+<div class="container-fluid section bg-secondary position-absolute"></div>
+
+<div class="container position-relative">
+    <div class="row">
+        <h2 class=" mt-5">Aktiviteter</h2>
+        <div class="col-12 col-lg-6 mt-3 pe-4">
+            <a href="#"><img src="img/image.webp" class="img-fluid w-100"></a>
+        </div>
+        <div class="col-12 col-lg-6 mt-3 ps-4">
+            <a href="#" class="link-dark"><h2>Titel på aktiviteten</h2></a>
+            <span>23-06-2024</span>
+            <p class="mt-2">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s. Ipsum passages, and more recently with desktop.</p>
+            <a href="#">Læs mere</a>
+        </div>
+        <div class="col-12 col-lg-4 mt-5 mt-lg-4 d-flex flex-column position-relative">
+            <img src="img/image.webp" class="img-fluid w-100">
+            <h2 class="mt-3">Titel på aktiviteten</h2>
+            <span>23-06-2024</span>
+            <a href="#" class="stretched-link mt-1">Læs mere</a>
+        </div>
+        <div class="col-12 col-lg-4 mt-5 mt-lg-4 d-flex flex-column position-relative">
+            <img src="img/image.webp" class="img-fluid w-100">
+            <h2 class="mt-3">Titel på aktiviteten</h2>
+            <span>23-06-2024</span>
+            <a href="#" class="stretched-link mt-1">Læs mere</a>
+        </div>
+        <div class="col-12 col-lg-4 mt-5 mt-lg-4 d-flex flex-column position-relative">
+            <img src="img/image.webp" class="img-fluid w-100">
+            <h2 class="mt-3">Titel på aktiviteten</h2>
+            <span>23-06-2024</span>
+            <a href="#" class="stretched-link mt-1">Læs mere</a>
+        </div>
+    </div>
+</div>
+
 <br>
 <br>
 <br>
 <br>
+<br><p>Hallo</p>
 <br>
 <br>
 <br>
 <br>
-<br>
+
 <br>
 <br>
 <br>
