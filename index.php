c800e4f94af31c862f3987906e7027d683dd5771
Sebastian <sec001@edu.zealand.dk>
2024-05-24 12:08:18 +0200

    video update

--- index.php
+++ index.php
@@ -4,17 +4,17 @@ require "settings/init.php";
 <!DOCTYPE html>
 <html lang="da">
 <head>
-	<meta charset="utf-8">
-	
-	<title>Sigende titel</title>
-	
-	<meta name="robots" content="All">
-	<meta name="author" content="Udgiver">
-	<meta name="copyright" content="Information om copyright">
-	
-	<link href="css/styles.css" rel="stylesheet" type="text/css">
-	
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8">
+
+    <title>Sigende titel</title>
+
+    <meta name="robots" content="All">
+    <meta name="author" content="Udgiver">
+    <meta name="copyright" content="Information om copyright">
+
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -28,14 +28,19 @@ require "settings/init.php";
     <?php include("header.php"); ?>
 </header>
 
-
-<div class="container-fluid hero bg-primary d-flex align-items-center">
-    <div class="container text-light">
+<div class="hero d-flex align-items-center bg-primary">
+    <div class="container text-light z-1">
         <h1>DEN GODE TITEL<br>PÅ SKÆRMEN HER</h1>
         <p class="mt-4">Få overblik over <a href="activities.php" class="link-light link-opacity-75-hover">aktiviteter</a><br>og <a href="attractions.php" class="link-light link-opacity-75-hover">attraktioner</a> til dit ophold</p>
     </div>
+    <div class="position-absolute">
+        <div class="position-absolute w-100 h-100 bg-primary bg-opacity-85"></div>
+        <video autoplay muted loop>
+            <source src="video.mp4" type="video/mp4">
+        </video>
+    </div>
 </div>
-<p>test</p>
+
 <div class="container-fluid section-index bg-secondary position-absolute"></div>
 
 <div class="container position-relative">
@@ -75,27 +80,27 @@ require "settings/init.php";
     <div class="row">
         <h2 class="mt-5">Attraktioner</h2>
         <div class="col-12 col-lg-6 mt-3 pe-lg-4">
-            <a href="#"><img src="img/image.webp" class="img-fluid w-100"></a>
+            <a href="attraction.php"><img src="img/image.webp" class="img-fluid w-100"></a>
         </div>
         <div class="col-12 col-lg-6 mt-3 ps-lg-4">
             <a href="#" class="link-dark"><h2>Titel på aktiviteten</h2></a>
             <p class="mt-3">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s. Ipsum passages, and more recently with desktop.</p>
-            <a href="#">Læs mere</a>
+            <a href="attraction.php">Læs mere</a>
         </div>
         <div class="col-12 col-lg-4 mt-5 mt-lg-4 d-flex flex-column position-relative">
             <img src="img/image.webp" class="img-fluid w-100">
             <h2 class="mt-3">Titel på aktiviteten</h2>
-            <a href="#" class="stretched-link">Læs mere</a>
+            <a href="attraction.php" class="stretched-link">Læs mere</a>
         </div>
         <div class="col-12 col-lg-4 mt-5 mt-lg-4 d-flex flex-column position-relative">
             <img src="img/image.webp" class="img-fluid w-100">
             <h2 class="mt-3">Titel på aktiviteten</h2>
-            <a href="#" class="stretched-link">Læs mere</a>
+            <a href="attraction.php" class="stretched-link">Læs mere</a>
         </div>
         <div class="col-12 col-lg-4 mt-5 mt-lg-4 d-flex flex-column position-relative">
             <img src="img/image.webp" class="img-fluid w-100">
             <h2 class="mt-3">Titel på aktiviteten</h2>
-            <a href="#" class="stretched-link">Læs mere</a>
+            <a href="attraction.php" class="stretched-link">Læs mere</a>
         </div>
     </div>
 </div>
